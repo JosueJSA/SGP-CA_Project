11a8dfa2c7f5ee0810efad38f37fcf2bebdfcb01
Josué Sangabriel Alarcón; <josuesa090@gmail.com;>
2021-04-20 12:07:36 -0500

    Change JfileChooser from swing to FileChooser from javaFX

--- SGP-CA_Project/src/sgp/ca/demodao/SGPCA_Project.java
+++ SGP-CA_Project/src/sgp/ca/demodao/SGPCA_Project.java
@@ -10,9 +10,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import sgp.ca.dataaccess.FtpClient;
 
 public class SGPCA_Project extends Application {
+    
     
     @Override
     public void start(Stage stage) throws Exception {
@@ -22,19 +22,11 @@ public class SGPCA_Project extends Application {
         
         stage.setScene(scene);
         stage.show();
+        
     }
 
     public static void main(String[] args){
-        launch(args);        
-        DialogBox testBox = new DialogBox();
-        
-        FtpClient connection = new FtpClient();
-        String file = connection.saveFileIntoFilesSystem(testBox.getFileSelectedPath(), testBox.getFileNameSelected());
-        System.out.println("Terminé de guardar");
-        connection.downloadFileFromFilesSystemByName(testBox.getFileNameSelected(), testBox.getDirectorySelectedPath());
-        System.out.println("Terminé recuperar");
-        connection.deleteFileFromFilesSystemByName(file);
-        System.out.println("Terminé de eliminar");
+        launch(args);
     }
     
 }
